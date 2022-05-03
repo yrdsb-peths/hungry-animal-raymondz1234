0a631c38429a75c01dbbde2e8353ca7e9a520c48
raymondz1234 <101139065+raymondz1234@users.noreply.github.com>
2022-05-03 13:51:13 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,8 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Jippo swag = new Jippo();
+        addObject(swag, getWidth() / 2, getHeight()/2);
+        
     }
 }
